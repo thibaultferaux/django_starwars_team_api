dc90f4fdd104bc266d68a283b1a08bf6a5f23711
Thibault <thibault.feraux@hotmail.com>
2025-08-22 22:07:48 +0200

    feat: add serializers for Character and Master models

---
+++ characters/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from .models import Character, Master
+
+
+class MasterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Master
+        fields = ['master_name']
+
+class CharacterListSerializer(serializers.ModelSerializer):
+    """Serializer for character list (minimal data)"""
+    class Meta:
+        model = Character
+        fields = ['id', 'name', 'image_url', 'species', 'homeworld']
+
+class CharacterDetailSerializer(serializers.ModelSerializer):
+    """Serializer for character detail view (full data)"""
+    affiliations = serializers.ListField(read_only=True)
+    masters = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Character
+        fields = [
+            'id', 'name', 'height', 'mass', 'gender', 'homeworld',
+            'species', 'image_url', 'affiliations', 'masters',
+            'created_at', 'updated_at'
+        ]
+
+    def get_masters(self, obj):
+        """Resturn a list of masters for the character."""
+        masters = Master.objects.filter(characters=obj)
+        return [master.master_name for master in masters]
